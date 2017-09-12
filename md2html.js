801d047858e130271dd35b4a8091c87632f1f7ac
dashidan <2903872229@qq.com>
2017-09-12 10:27:31 +0800

    删除node_modules

--- md2html.js
+++ md2html.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require('fs');
 const path = require("path");
-const showdown = require('showdown');
+const showdown = require('sinbad_showdown');
 const Handlebars = require("handlebars");
 
 /** Check for arguments*/
