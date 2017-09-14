161aa2b0d0179cd99961cc205596707ed839029e
dashidan <2903872229@qq.com>
2017-09-14 21:02:57 +0800

    加入git，excel，math，windows配置模板 修改批处理

--- convert_excel_basic.bat
+++ convert_excel_basic.bat
@@ -1,2 +1,2 @@
-node md2html.js D:\workplace\git\Doc\educate\excel\basic D:\workplace\git\Doc\dashidan.com\article\excel\basic Excel½Ì³Ì »ù´¡Æª excel basic
+node md2html.js D:\workplace\git\Doc\educate\excel\basic D:\workplace\git\Doc\dashidan.com\article\excel\basic excel basic
 pause
--- convert_git_basic.bat
+++ convert_git_basic.bat
@@ -1,2 +1,2 @@
-node md2html.js D:\workplace\git\Doc\educate\git\basic D:\workplace\git\Doc\dashidan.com\article\git\basic Git½Ì³Ì »ù´¡Æª git basic
+node md2html.js D:\workplace\git\Doc\educate\git\basic D:\workplace\git\Doc\dashidan.com\article\git\basic git basic
 pause
